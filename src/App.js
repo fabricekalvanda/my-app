7c0241a568135d91e9f0daa10bd65f3e5956bf00
fabricekalvanda <fabricekalvanda@gmail.com>
2024-01-12 17:16:56 -0800

    build functionallity with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Card from "./components/Cards";
+import "./App.css";
+
+const photos = [
+  'https://picsum.photos/id/1001/200/200', 
+  'https://picsum.photos/id/1002/200/200', 
+  'https://picsum.photos/id/1003/200/200', 
+  'https://picsum.photos/id/1004/200/200', 
+  'https://picsum.photos/id/1005/200/200', 
+  'https://picsum.photos/id/1006/200/200'
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container text-center mt-5">
+        <h1>Gallery</h1>
+        <div className="row" key={photos}>
+          {photos.map((photo) => <Card src={photo} />)}
+        </div>
+      </div>
+    </>
   );
 }
 
