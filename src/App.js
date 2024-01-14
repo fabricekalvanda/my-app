722fdce330513a2fd45499c6f99642d9493ab8a9
fabricekalvanda <fabricekalvanda@gmail.com>
2024-01-13 23:05:59 -0800

    adds form logic to upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Cards";
 import UploadForm from "./components/UploadForm";
@@ -6,17 +6,29 @@ import "./App.css";
 
 const photos = [
   "https://picsum.photos/id/1001/200/200",
-  "https://picsum.photos/id/1002/200/200",
-  "https://picsum.photos/id/1003/200/200",
-  "https://picsum.photos/id/1004/200/200",
-  "https://picsum.photos/id/1005/200/200",
-  "https://picsum.photos/id/1006/200/200",
 ];
 
 function App() {
+  const [count, setCount] = useState();
+  const [inputs, setInputs] = useState({ title: null, file: null, path: null });
   const [items, setItems] = useState(photos);
   const [isCollapsed, collapse] = useState(false);
   const toggle = () => collapse(!isCollapsed);
+  const handleOnChange = (e) => {
+    if (e.target.name === 'file') {
+      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+    } else {
+      setInputs({...inputs, title: e.target.value})
+    }
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setItems([inputs.path,...items])
+  }
+
+  useEffect(() => {
+    setCount(`you have ${items.length} image${items.length > 1 ? "s": " "}`)
+  }, [items])
 
   return (
     <>
@@ -26,7 +38,12 @@ function App() {
           {isCollapsed ? "Close" : "+ Add"}
         </button>
         <div className="clearfix mb-4"></div>
-        <UploadForm isVisible={isCollapsed} />
+        <UploadForm 
+          isVisible={isCollapsed}
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit} 
+        />
+        {count}
         <h1>Gallery</h1>
         <div className="row">
           {items.map((photo) => (
