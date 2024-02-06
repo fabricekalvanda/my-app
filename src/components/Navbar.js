1722bd3fc70fb01a4e56d16991211f2695fb15b8
fabricekalvanda <fabricekalvanda@gmail.com>
2024-02-06 00:45:42 -0800

    apply context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,7 +60,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Firestock
+          ⚡Firestock
         </a>
         <button
           className="navbar-toggler"
