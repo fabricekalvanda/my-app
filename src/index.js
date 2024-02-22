a11fc73b4b950dc4e9dd11e8fdcbf172d79d9334
fabricekalvanda <fabricekalvanda@gmail.com>
2024-02-22 12:21:37 -0800

    add route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Provider from './context/FirestoreContext';
-import AuthProvider from './context/AuthContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Provider from "./context/FirestoreContext";
+import AuthProvider from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Stocks() {
+  return <h1>My Stock Images</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <Provider>
         <Router>
+          <Routes>
+            <Route path="/" element={<App />}/>
+            <Route path="/stocks" element={<Stocks />} />
+          </Routes>
           <App />
         </Router>
       </Provider>
